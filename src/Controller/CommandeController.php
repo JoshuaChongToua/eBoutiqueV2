97993b334ca9410c286cd1065211fd3d176578dd
JoshuaChongToua <chongtouajoshua@gmail.com>
2025-05-09 17:47:28 +0200

    a

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -103,37 +103,12 @@ final class CommandeController extends AbstractController
     #[Route('/{id}', name: 'app_commande_show', methods: ['GET'])]
     public function show(Commande $commande): Response
     {
+        $produits = json_decode($commande->getProduit(), true);
+
         return $this->render('commande/show.html.twig', [
             'commande' => $commande,
+            'produits' => $produits,
         ]);
     }
 
-//    #[Route('/{id}/edit', name: 'app_commande_edit', methods: ['GET', 'POST'])]
-//    public function edit(Request $request, Commande $commande, EntityManagerInterface $entityManager): Response
-//    {
-//        $form = $this->createForm(CommandeType::class, $commande);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $entityManager->flush();
-//
-//            return $this->redirectToRoute('app_commande_index', [], Response::HTTP_SEE_OTHER);
-//        }
-//
-//        return $this->render('commande/edit.html.twig', [
-//            'commande' => $commande,
-//            'form' => $form,
-//        ]);
-//    }
-
-//    #[Route('/{id}', name: 'app_commande_delete', methods: ['POST'])]
-//    public function delete(Request $request, Commande $commande, EntityManagerInterface $entityManager): Response
-//    {
-//        if ($this->isCsrfTokenValid('delete' . $commande->getId(), $request->getPayload()->getString('_token'))) {
-//            $entityManager->remove($commande);
-//            $entityManager->flush();
-//        }
-//
-//        return $this->redirectToRoute('app_commande_index', [], Response::HTTP_SEE_OTHER);
-//    }
 }
