87b3151ce8aada93a0787337d98ec0e4706ddaaa
JoshuaChongToua <chongtouajoshua@gmail.com>
2025-05-10 16:05:34 +0200

    a

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class SecurityController extends AbstractController
 {
